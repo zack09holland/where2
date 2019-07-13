eeb99b314a7d9dfd399d6671cd006ec2e75e1469
devcwilkey <DevCWilkey@gmail.com>
2019-07-13 10:47:41 -0700

    Example of Ajax to Query cities for Zomato

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,39 @@
 /// Javascript Code Below
 var that = this;
-var searchParams = {};
+var where2Application = {
+    searchParams: {
+        start : "",
+        end : "",
+        destination : "",
+        radius : 0
+    },
+    zomatoApi : {
+        searchParams : {
+            url: "https://developers.zomato.com/api/v2.1/cities?",
+            count: 10
+        },
+        queryZomato : function () {
+            queryUrl = this.searchParams.url + "q=" + that.where2Application.searchParams.destination + "&count=" + this.searchParams.count
+            $.ajax({
+                headers: {
+                    'user-key':'03136f6b258dbdebd5478a174180a71f'
+                },
+                url: queryUrl,
+                method: "get"
+            }).then(function(data){
+                console.log(data)
+            });
+        }
+    }
+};
 $('#submit').on("click", function(){
-    that.searchParams = {
+    that.where2Application.searchParams = {
         start :  $('#start').val().trim(),
         end: $('#end').val().trim(),
         destination: $('#destination').val().trim(),
         radius: $('#radius').val().trim()
     }
-    console.log(
-        that.searchParams
-    )
+    that.where2Application.zomatoApi.queryZomato()
+
+
 })
