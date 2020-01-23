5526898d943a7dd7128c63902386a9d797663a69
Zach Holland <zack09holland@gmail.com>
2020-01-22 17:02:57 -0800

    changing files to be served via node

---
+++ app/public/javascript/init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBNpstskQf4JklIHw__h70G8JBi_ZvCSZc",
+    authDomain: "where2-hollyw00d.firebaseapp.com",
+    databaseURL: "https://where2-hollyw00d.firebaseio.com",
+    projectId: "where2-hollyw00d",
+    storageBucket: "",
+    messagingSenderId: "714594318842",
+    appId: "1:714594318842:web:502c8665f14c0695"
+};
+  
+// Initialize Firebase
+var defaultApplication = firebase.initializeApp(firebaseConfig);
