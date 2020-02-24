9250306ee4d4b06b7bdc042e298f388e7f7ff54b
Zach Holland <zack09holland@gmail.com>
2020-02-23 19:50:57 -0800

    changed html routes as they werent working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,8 +15,8 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 
   // If no matching route is found default to home
