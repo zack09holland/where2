a32281379415d9686dd79f67604289b2d168a2f6
Zach Holland <zack09holland@gmail.com>
2020-02-23 21:00:28 -0800

    changing content display

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -668,4 +668,22 @@ firebase.auth().onAuthStateChanged(function(user) {
                 })(i);
             }
         }
+        
+        //class manipulations - needed if classList is not supported
+        //https://jaketrent.com/post/addremove-classes-raw-javascript/
+        function hasClass(el, className) {
+              if (el.classList) return el.classList.contains(className);
+              else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+        }
+        function addClass(el, className) {
+             if (el.classList) el.classList.add(className);
+             else if (!hasClass(el, className)) el.className += " " + className;
+        }
+        function removeClass(el, className) {
+              if (el.classList) el.classList.remove(className);
+              else if (hasClass(el, className)) {
+                var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                el.className=el.className.replace(reg, ' ');
+              }
+        }
     })();
