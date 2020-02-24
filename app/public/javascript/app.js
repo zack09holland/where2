5af93e6c206910ef4005bd550dd2c369a9487167
Zach Holland <zack09holland@gmail.com>
2020-02-23 20:50:07 -0800

    changing content display

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -646,3 +646,26 @@ firebase.auth().onAuthStateChanged(function(user) {
       });
     }
   
+    (function(){
+        var panelTriggers = document.getElementsByClassName('js-cd-panel-trigger');
+        if( panelTriggers.length > 0 ) {
+            for(var i = 0; i < panelTriggers.length; i++) {
+                (function(i){
+                    var panelClass = 'js-cd-panel-'+panelTriggers[i].getAttribute('data-panel'),
+                        panel = document.getElementsByClassName(panelClass)[0];
+                    // open panel when clicking on trigger btn
+                    panelTriggers[i].addEventListener('click', function(event){
+                        event.preventDefault();
+                        addClass(panel, 'cd-panel--is-visible');
+                    });
+                    //close panel when clicking on 'x' or outside the panel
+                    panel.addEventListener('click', function(event){
+                        if( hasClass(event.target, 'js-cd-close') || hasClass(event.target, panelClass)) {
+                            event.preventDefault();
+                            removeClass(panel, 'cd-panel--is-visible');
+                        }
+                    });
+                })(i);
+            }
+        }
+    })();
